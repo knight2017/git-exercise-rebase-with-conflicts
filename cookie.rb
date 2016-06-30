ec03d66b748af43efa41b0f8e0f8133dba38a881
Wei Yi <weiyi2017@gmail.com>
2016-06-30 15:33:39 -0700

    sda

--- cookie.rb
+++ cookie.rb
@@ -1,12 +1,15 @@
 class Cookie
   attr_accessor :sugar
   attr_accessor :flour
+  attr_accessor :butter
 
+<<<<<<< 0bbdd7376af47ae755994d1b6f77a16684a879ac
   SUGAR_CALORIES = 3.87
   FLOUR_CALORIES = 3.64
 
   def initialize(sugar, flour)
     @sugar, @flour = sugar, flour
+
   end
 
   def calorie_count
